eeb36bf4362c2a5de3a9fa51201d0e283466e5e5
SimonS2019 <shentiexiong@gmail.com>
2024-10-01 11:24:00 +1300

    Add FormsModule to imports in app.module.ts

--- hostinger-ng-test/src/app/app.module.ts
+++ hostinger-ng-test/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WriteNotesComponent } from './write-notes/write-notes.component';
 import { DisplayNotesComponent } from './display-notes/display-notes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { DisplayNotesComponent } from './display-notes/display-notes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule // Add FormsModule to imports
   ],
   providers: [],
   bootstrap: [AppComponent]
