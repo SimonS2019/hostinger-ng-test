2427194819a7c213313b72c468a2c06f4f617b1b
SimonS2019 <shentiexiong@gmail.com>
2024-10-01 11:24:10 +1300

    Refactor write-notes component HTML and TypeScript files

--- hostinger-ng-test/src/app/write-notes/write-notes.component.ts
+++ hostinger-ng-test/src/app/write-notes/write-notes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NotesService } from '../notes.service';
 
 @Component({
   selector: 'app-write-notes',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./write-notes.component.css']
 })
 export class WriteNotesComponent {
+  newNote: string = '';
 
+  constructor(private notesService: NotesService) {}
+
+  addNote() {
+    if (this.newNote.trim()) {
+      this.notesService.addNote(this.newNote.trim());
+      this.newNote = '';
+    }
+  }
 }
