5f8f0ca0929aff9c85fdfc33e6aa2d331e32177e
SimonS2019 <shentiexiong@gmail.com>
2024-10-01 11:25:34 +1300

    Refactor display-notes component to show a list of notes

--- hostinger-ng-test/src/app/display-notes/display-notes.component.ts
+++ hostinger-ng-test/src/app/display-notes/display-notes.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotesService } from '../notes.service';
 
 @Component({
   selector: 'app-display-notes',
   templateUrl: './display-notes.component.html',
   styleUrls: ['./display-notes.component.css']
 })
-export class DisplayNotesComponent {
+export class DisplayNotesComponent implements OnInit {
+  notes: string[] = [];
 
+  constructor(private notesService: NotesService) {}
+
+  ngOnInit() {
+    this.notes = this.notesService.getNotes();
+  }
 }
