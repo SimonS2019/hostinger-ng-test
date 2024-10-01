0daeca8a6f36e5d7bbed2525b46ca72c7f86e2c8
SimonS2019 <shentiexiong@gmail.com>
2024-10-01 14:20:51 +1300

    Refactor gallery component to remove video support

--- hostinger-ng-test/src/app/gallery/gallery.component.ts
+++ hostinger-ng-test/src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { imagePaths } from './image';
 
 @Component({
   selector: 'app-gallery',
@@ -9,24 +10,6 @@ export class GalleryComponent implements OnInit {
   mediaList: { type: string; url: string; description: string }[] = [];
 
   ngOnInit() {
-    // Example media URLs with descriptions
-    this.mediaList = [
-      {
-        type: 'image',
-        url: 'https://drive.google.com/drive-viewer/AKGpihaaNYWyOWdtwFSTKcdwuI3ML3fd85wxfXJzzCeAcGAyMO7UGJ4vwhH61Wq_04BlKAoNnfXtaOesuXnzNSNYIkfYxPYUydKzXIw=s1600-rw-v1',
-        description: 'Image 0 description',
-      },
-      {
-        type: 'image',
-        url: 'https://drive.google.com/drive-viewer/AKGpihY41GJzj1QEbv49Ekb2XVjWpLTDR2PemRWoAoX7ptfht8lS-f7Pds0C2C741UbUHh4Mc7o8sDwlPIFLtJ3AtOexgvKl-H0jc88=s1600-rw-v1',
-        description: 'Image 1 description',
-      },
-      {
-        type: 'image',
-        url: 'https://drive.google.com/drive-viewer/AKGpihYw50JLYCtGqEPGeM3FQPkOleG0--PbMRfog33ZWbXbBkK60d3dtdwM2jFujtiOG02UiQ-Bs2Yb5EIl119W4prDW5KZbCO_9x8=s1600-rw-v1',
-        description: 'Image 2 description',
-      },
-      // { type: 'video', url: 'https://example.com/path/to/your/video2.mp4', description: 'Video 2 description' }
-    ];
+    this.mediaList = imagePaths;
   }
 }
---
+++ hostinger-ng-test/src/app/gallery/image.ts
@@ -0,0 +1,143 @@
+// image.ts
+export const imagePaths = [
+    {
+      type: 'image',
+      url: 'assets/images/Frank_and_Lily_beach.webp',
+      description: 'Frank and Lily at the beach',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Frank_and_Lily_coffee.webp',
+      description: 'Frank and Lily having coffee',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Frank_and_Lily_dining_2.webp',
+      description: 'Frank and Lily dining 2',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Frank_and_Lily_dining.webp',
+      description: 'Frank and Lily dining',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Frank_and_Lily_running.webp',
+      description: 'Frank and Lily running',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Frank_and_Lily_sightseeing.webp',
+      description: 'Frank and Lily sightseeing',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Frank_and_Lily_travel.webp',
+      description: 'Frank and Lily travel',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Frank_and_Lily_traveling.webp',
+      description: 'Frank and Lily traveling',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Hammer_in_basket.webp',
+      description: 'Hammer in basket',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Hammer_in_closet.webp',
+      description: 'Hammer in closet',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Hammer_in_container.webp',
+      description: 'Hammer in container',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Hammer_on_book_front.webp',
+      description: 'Hammer on book front',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Hammer_on_book_side.webp',
+      description: 'Hammer on book side',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Hammer_on_sofa.webp',
+      description: 'Hammer on sofa',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Hammer_paw_closeup.webp',
+      description: 'Hammer paw closeup',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Hammer_playing_on_sofa.webp',
+      description: 'Hammer playing on sofa',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Hammer_sunbathing.webp',
+      description: 'Hammer sunbathing',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_cafe.webp',
+      description: 'Lily at cafe',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_graduation.webp',
+      description: 'Lily graduation',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_holding_baby.webp',
+      description: 'Lily holding baby',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_in_flower_shop.webp',
+      description: 'Lily in flower shop',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_on_street.webp',
+      description: 'Lily on street',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_outdoors.webp',
+      description: 'Lily outdoors',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_pouring_coffee.webp',
+      description: 'Lily pouring coffee',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_street.webp',
+      description: 'Lily on street',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_with_chick.webp',
+      description: 'Lily with chick',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_with_lamb.webp',
+      description: 'Lily with lamb',
+    },
+    {
+      type: 'image',
+      url: 'assets/images/Lily_with_sheep.webp',
+      description: 'Lily with sheep',
+    },
+  ];
