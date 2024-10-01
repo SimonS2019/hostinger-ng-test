9396e386a41473b6d1ee4c0ded4d6f5c49a29377
SimonS2019 <shentiexiong@gmail.com>
2024-10-01 14:45:06 +1300

    Refactor search-results component to filter notes based on search query

--- hostinger-ng-test/src/app/search-results/search-results.component.ts
+++ hostinger-ng-test/src/app/search-results/search-results.component.ts
@@ -6,16 +6,19 @@ import { Note } from '../models/note.model'; // Import the Note interface
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.css']
+  styleUrls: ['./search-results.component.css'],
 })
 export class SearchResultsComponent implements OnInit {
   searchQuery: string = '';
   filteredNotes: Note[] = []; // Ensure filteredNotes is typed as an array of Note
 
-  constructor(private route: ActivatedRoute, private notesService: NotesService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private notesService: NotesService
+  ) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.searchQuery = params['query'] || '';
       this.filterNotes();
     });
@@ -23,11 +26,20 @@ export class SearchResultsComponent implements OnInit {
 
   filterNotes() {
     const allNotes = this.notesService.getNotes();
-    this.filteredNotes = allNotes.filter(note => 
-      note.title.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      note.subtitle.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      note.details.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      note.creator.toLowerCase().includes(this.searchQuery.toLowerCase())
+    this.filteredNotes = allNotes.filter(
+      (note) =>
+        (note.title &&
+          note.title.toLowerCase().includes(this.searchQuery.toLowerCase())) ||
+        (note.subtitle &&
+          note.subtitle
+            .toLowerCase()
+            .includes(this.searchQuery.toLowerCase())) ||
+        (note.details &&
+          note.details
+            .toLowerCase()
+            .includes(this.searchQuery.toLowerCase())) ||
+        (note.creator &&
+          note.creator.toLowerCase().includes(this.searchQuery.toLowerCase()))
     );
   }
 }
